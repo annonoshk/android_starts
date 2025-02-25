e1d84e0101da71aeca3e5910e4fd7bd4a9f24c20
Huck Kim <huck.kim@kangwon.ac.kr>
2025-02-25 12:28:31 +0900

    1st commit

--- app/src/main/java/com/crevolika/jetpack01/MainActivity.kt
+++ app/src/main/java/com/crevolika/jetpack01/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.crevolika.jetpack01
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.squareup.picasso.Callback
+import com.squareup.picasso.MemoryPolicy
+import com.squareup.picasso.NetworkPolicy
 import com.squareup.picasso.Picasso
+import jp.wasabeef.picasso.transformations.BlurTransformation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +34,48 @@ class MainActivity : AppCompatActivity() {
         Picasso.get()
             .load(logoUrl)
             .fit().centerCrop()
+            .rotate(90.0f)
+            .transform(BlurTransformation(this))
             .into(myImageView)
+
+/*        Picasso.get()
+            .load(logoUrl)
+            .placeholder(R.drawable.placeholder)
+            .error(R.drawable.error)
+            .into(myImageView)*/
+
+        // callback
+/*        Picasso.get()
+            .load(logoUrl)
+            .placeholder(R.drawable.placeholder)
+            .error(R.drawable.error)
+            .into(myImageView, object : Callback {
+                override fun onSuccess() {
+                    Toast.makeText(this@MainActivity, "Success!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                override fun onError(e: Exception?) {
+                    Toast.makeText(this@MainActivity, "Error!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })*/
+            // no cache, no store, Offline
+/*            .networkPolicy(NetworkPolicy.NO_CACHE, NetworkPolicy.NO_STORE)
+            .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)*/
+
+        //setSpan(Object span, int start, int end, int flags)
+        val text = "Practice makes progress. Start now!"
+        val spannableString = SpannableString(text)
+
+        val start = 25 // Start index of the text portion
+        val end = 35  // End index of the text portion
+        val color = Color.RED // Color for the text portion
+
+        val colorSpan = ForegroundColorSpan(color)
+        spannableString.setSpan(colorSpan, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+
+        sampleSpanColorTextView.text = spannableString //sets text to TextView
 
     }
 }
